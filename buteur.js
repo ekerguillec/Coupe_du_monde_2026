e1f64019251dbb462038280c77ccfb54d8d66d7c
ekerguillec <ekerguillec@ensc.fr>
2026-06-05 18:22:50 +0200

    mise en place du carroussel des  meilleurs buteurs

--- buteur.js
+++ buteur.js
@@ -1,4 +1,4 @@
 const listeButeurs = [
     { nom: "C. Ronaldo", pays: "Portugal", image: "images/CR7.png", buts: 0 },
-    { nom: "Neymar", pays: "Brésil", image: "./images/Neymar.png", buts: 0 }
-]
+    { nom: "Neymar", pays: "Brésil", image: "./images/Neymar.png", buts: 0 },
+    ]
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mois = String(d.getMonth() + 1).padStart(2,'0') // +1 car janvier = 0, fé
 const jours = String(d.getDate()).padStart(2,'0') // padStart(2, '0') permet d'afficher 2 chiffres et que 6 soit sous la forme 06
 const annee = d.getFullYear()
 const date_actuelle = '06/11/2026' // mois + '/' + jours + '/' + annee  //'06/11/2026' pour tester la fonction avec une date précise ; 
+const duree_intervalle = 5000
 
 
 
@@ -50,3 +51,28 @@ const divButeurs = document.getElementById('liste_buteurs')
     listeButeurs.forEach(joueur => {
         divButeurs.innerHTML += `<div class="carte-joueur"> <img src="${joueur.image}"> <p id="nom_joueur"> ${joueur.nom} </p> <p id="pays_joueur"> ${joueur.pays}</p> <p id="score_buteur"> ${joueur.buts}</p></div>`
 })
+
+let indexActuel = 0
+
+function afficherJoueur(index) {
+    const cartes = document.querySelectorAll('.carte-joueur')
+    cartes.forEach(carte => carte.style.display = 'none')
+    cartes[index].style.display = 'block'
+}
+
+setInterval(() => {
+    indexActuel = (indexActuel + 1) % listeButeurs.length
+    afficherJoueur(indexActuel)
+}, duree_intervalle)
+
+document.getElementById('btn-next').addEventListener('click', () => {
+    indexActuel = (indexActuel + 1) % listeButeurs.length
+    afficherJoueur(indexActuel)
+})
+
+document.getElementById('btn-prev').addEventListener('click', () => {
+    indexActuel = (indexActuel - 1 + listeButeurs.length) % listeButeurs.length
+    afficherJoueur(indexActuel)
+})
+
+afficherJoueur(indexActuel)
